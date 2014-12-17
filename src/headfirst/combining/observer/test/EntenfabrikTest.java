4eaf6a7265bb8f19ce2259230495a646ff4deb21
spolydor-tgm <spolydor@student.tgm.ac.at>
2014-12-17 08:41:31 +0100

    added testcases

--- src/headfirst/combining/observer/test/EntenfabrikTest.java
+++ src/headfirst/combining/observer/test/EntenfabrikTest.java
@@ -1,32 +1,37 @@
 package headfirst.combining.observer.test;
 
+import headfirst.combining.observer.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class EntenfabrikTest {
+import static junit.framework.TestCase.assertEquals;
+
+public class EntenfabrikTest extends Entenfabrik {
+
+	private Entenfabrik entenfabrik;
 
 	@Before
 	public void setUp() throws Exception {
-
+		entenfabrik = new Entenfabrik();
 	}
 
 	@Test
 	public void testErzeugeStockEnte() throws Exception {
-
+		assertEquals(new StockEnte().toString(), entenfabrik.erzeugeStockEnte().toString());
 	}
 
 	@Test
 	public void testErzeugeMoorEnte() throws Exception {
-
+		assertEquals(new MoorEnte().toString(), entenfabrik.erzeugeMoorEnte().toString());
 	}
 
 	@Test
 	public void testErzeugeLockPfeife() throws Exception {
-
+		assertEquals(new LockPfeife().toString(), entenfabrik.erzeugeLockPfeife().toString());
 	}
 
 	@Test
 	public void testErzeugeGummiEnte() throws Exception {
-
+		assertEquals(new GummiEnte().toString(), entenfabrik.erzeugeGummiEnte().toString());
 	}
 }
