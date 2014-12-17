d3587304dde5507adff9d7434c462ef0a0c6aeca
spolydor-tgm <spolydor@student.tgm.ac.at>
2014-12-17 08:24:58 +0100

    added package test

---
+++ src/headfirst/combining/observer/test/EntenfabrikTest.java
@@ -0,0 +1,32 @@
+package headfirst.combining.observer.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EntenfabrikTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testErzeugeStockEnte() throws Exception {
+
+	}
+
+	@Test
+	public void testErzeugeMoorEnte() throws Exception {
+
+	}
+
+	@Test
+	public void testErzeugeLockPfeife() throws Exception {
+
+	}
+
+	@Test
+	public void testErzeugeGummiEnte() throws Exception {
+
+	}
+}
