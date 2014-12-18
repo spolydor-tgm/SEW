124b9a5e8ea3d8f20eb179c92bbad08ae9f725a0
spolydor-tgm <spolydor@student.tgm.ac.at>
2014-12-18 20:04:16 +0100

    update test cases

--- src/headfirst/combining/observer/test/StockEnteTest.java
+++ src/headfirst/combining/observer/test/StockEnteTest.java
@@ -1,10 +1,14 @@
 package headfirst.combining.observer.test;
 
+import headfirst.combining.observer.Quakologe;
 import headfirst.combining.observer.StockEnte;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class StockEnteTest {
 
@@ -13,26 +17,33 @@ public class StockEnteTest {
 
 	@Before
 	public void setUp() {
-
+		stockEnte = new StockEnte();
+		outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
 	}
 
 	@Test
 	public void testQuaken() throws Exception {
-
+		stockEnte.quaken();
+		assertEquals("Quak", outContent.toString().trim());
 	}
 
 	@Test
 	public void testRegistriereBeobachter() throws Exception {
-
+		stockEnte.registriereBeobachter(new Quakologe());
+		stockEnte.benachrichtigeBeobachtende();
+		assertEquals("Quakologe: Stockente hat gerade gequakt.", outContent.toString().trim());
 	}
 
 	@Test
 	public void testBenachrichtigeBeobachtende() throws Exception {
-
+		stockEnte.registriereBeobachter(new Quakologe());
+		stockEnte.benachrichtigeBeobachtende();
+		assertEquals("Quakologe: Stockente hat gerade gequakt.", outContent.toString().trim());
 	}
 
 	@Test
 	public void testToString() throws Exception {
-
+		assertEquals("Stockente", stockEnte.toString());
 	}
 }
